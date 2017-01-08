33912f177e387419875870c28dd6d9dd323b2bd7
Nabil Chatbi <scinfu@gmail.com>
2017-01-08 15:58:21 +0100

    no message

--- js/My2.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-    jQuery(function($){
-             $( '.menu-btn' ).click(function(){
-             $('.responsive-menu').toggleClass('expand')
-             })
-        })
-</script>
